9323c0ad7ab6b053dbbd47775b4e7c5a04f33b2e
markguti97 <82702906+markguti97@users.noreply.github.com>
2021-10-05 18:12:21 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -18,26 +18,38 @@ const App = () => {
 
     const equal = () => {                                   // eredményszámolás
         if(result !== "Error"){
-        try{
-            setResult(eval(result).toString());
-        }catch(err) {
-            setResult("Error")
+            try{
+                setResult(eval(result).toString());
+            }catch(err) {
+                setResult("Error")
         }}
         else    
             setResult("Error")
     }
 
     const clear = () => {                                   // teljes kijelző törlése
-        setResult("");
+        setResult(""); 
     }
 
     const backspace = () => {                               // egy-egy karakter törlése a kijelzőről
-        setResult(result.slice(0, result.length - 1));
+        if(result === "Error")
+        setResult("");                
+        else                                         
+            setResult(result.slice(0, result.length - 1));
     }
 
-    const loadIn = () => {                                              // külön fájlban eltárolt szám betöltése a kijelzőbe
-        axios.get("/loadin").then((response) => {                       
-            setResult(response.data.savedNumber.toString());
+    const loadIn = () => {                                             // külön fájlban eltárolt szám betöltése a kijelzőbe
+        
+        axios.get("/loadin").then((response) => {  
+                    if(result !== "Error"){
+                        if(result.concat(response.data.savedNumber.toString()).length <= 15){
+                            setResult(result.concat(response.data.savedNumber.toString()));
+                        }
+                    }
+                    else{
+                        setResult(response.data.savedNumber.toString())
+                    }
+            
           });
     }
 
@@ -53,56 +65,63 @@ const App = () => {
                     reject("Error")
                 }
             })
-            equalAndFetch.then((message) => {                                    // valid érték esetén elküldjük a tárolni kívánt számot/eredményt a szervernek
+            equalAndFetch.then((message) => {                        // valid érték esetén elküldjük a tárolni kívánt számot/eredményt a szervernek
                 fetch("/store", {
-                method: "POST",
-                headers: {
+                    method: "POST",
+                    headers: {
                     'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
+                        },
+                    body: JSON.stringify({
                     savedNumber: JSON.parse(message)
+                    })
                 })
-            })
-        }).catch((message) => {
-            setResult(message)})               
-    }
+                }).catch((message) => {
+                    setResult(message)})               
+                }
     
     return (        
         <div className="container">
             <form>
-                <input type="text" defaultValue={result} />
+                <input id="screen" type="text" defaultValue={result} />
             </form>
 
             <div id="logo"><img src="logo.png"/></div>
-                <div className="keypad">     
-
-                    <button className="highlight2" onClick={storeNumber} id="store">Store</button>
+                <div className="keypad">                   
 
                     <button className="highlight" onClick={clear} id="clear">AC</button>                    
                     <button className="highlight" onClick={backspace} id="backspace">C</button>
+                    <button className="highlight" name="(" onClick={handleClick}>(</button>
+                    <button className="highlight" name=")" onClick={handleClick}>)</button>
 
-                    <button className="highlight" name="/" onClick={handleClick}> &#xf7;</button>
                     <button name="7" onClick={handleClick}>7</button>
                     <button name="8" onClick={handleClick}>8</button>
                     <button name="9" onClick={handleClick}>9</button>
-                    <button className="highlight" name="*" onClick={handleClick}>&#215;</button>
+
+                    <button className="highlight" name="/" onClick={handleClick}> &#xf7;</button>
+
                     <button name="4" onClick={handleClick}>4</button>
                     <button name="5" onClick={handleClick}>5</button>
                     <button name="6" onClick={handleClick}>6</button>
-                    <button className="highlight" name="-" onClick={handleClick}>-</button>
+
+                    <button className="highlight" name="*" onClick={handleClick}>&#215;</button>
+
                     <button name="1" onClick={handleClick}>1</button>
                     <button name="2" onClick={handleClick}>2</button>
                     <button name="3" onClick={handleClick}>3</button>
-                    <button className="highlight" name="+" onClick={handleClick}>+</button>
+
+                    <button className="highlight" name="-" onClick={handleClick}>-</button>
+
                     <button name="0" onClick={handleClick}>0</button>
                     <button name="." onClick={handleClick}>.</button>
 
-                    <button className="highlight2" onClick={loadIn} id="loadin">Load in</button>
                     <button className="highlight" onClick={equal} id="equal">=</button>
+                    <button className="highlight" name="+" onClick={handleClick}>+</button>
+
+                    <button className="highlight2" onClick={storeNumber} id="store">Store</button>
+                    <button className="highlight2" onClick={loadIn} id="loadin">Load in</button>
 
                 </div>
         </div>
     );
 }
-
 export default App;
